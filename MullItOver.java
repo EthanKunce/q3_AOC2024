c0de17a41dc5ef3e63d6a0788425f94b612079fc
Ethan Kunce <ethankunce@ymail.com>
2024-12-03 12:22:59 -0500

    first commit

---
+++ MullItOver.java
@@ -0,0 +1,17 @@
+/*
+ * Author:		Ethan Kunce
+ * Date:		03 December 2024
+ * ProgramName:	MullItOver
+ * Desc:		Boiler Plate Text
+ */
+
+public class MullItOver {
+    
+    public static void main(String [] args)
+    {
+        
+        
+        
+    } // End Main Method
+    
+} // End Class
